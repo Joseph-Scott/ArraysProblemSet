ea80e55efc18d945eacbbdb1d80ec884f1509fa1
Joseph Scott <jbs198585@gmail.com>
2019-07-06 14:16:08 -0400

    problems

---
+++ arrays.js
@@ -0,0 +1,24 @@
+// 1. PrintReverse()
+// Write a function printReverse() that takes an array as an argument and prints out the elements in the array in reverse order
+// (don't actually reverse the array itself)
+
+
+
+
+// 2. isUniform()
+// Write a function isUniform() which takes an array as an argument
+// and returns true if all elements in the array are identical
+
+
+
+
+// 3. sumArray()
+// Write a function sumArray() that accepts an array of numbers and
+// returns the sum of all numbers in the array
+
+
+
+
+// 4. max()
+// Write a function max() that accepts an array of numbers and
+// returns the maximum number in the array
